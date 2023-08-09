5e2b01b646886f936f5cf8ba2a559695153045ac
fcarrillos <permat75@gmail.com>
2023-08-09 07:16:43 +0200

    creando el template de registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -16,7 +16,7 @@
                 </h1>
                 <nav class="flex gap-2 items-center">
                     <a href="" class="font-bolt uppercase text-gray-600 text-sm">Login</a>
-                    <a href="" class="font-bolt uppercase text-gray-600 text-sm">Crear cuenta</a>
+                    <a href="/crear-cuenta" class="font-bolt uppercase text-gray-600 text-sm">Crear cuenta</a>
                 </nav>
             </div>
         </header>
